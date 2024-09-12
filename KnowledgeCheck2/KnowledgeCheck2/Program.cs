17ebd0ce5068397274adfa04238fa10c7e81383f
Jacobi Wilson <jacobiwilson123@gmail.com>
2024-09-12 17:09:51 -0400

    Updated Program.cs with new functionality

---
+++ KnowledgeCheck2/KnowledgeCheck2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How many records do you want to add? ");
+        var numberOfRecords = int.Parse(Console.ReadLine());
+
+        var recordList = new List<MyBaseClass>();
+        for (int i = 0; i < numberOfRecords; i++)
+        {
+            // In this loop, populate the object's properties using Console.ReadLine()
+            var myObject = new MyInheritedClass();
+
+            Console.WriteLine("Enter the value for myProperty: ");
+            myObject.myProperty = Console.ReadLine();
+
+            Console.WriteLine("Enter the value for inheritedProperty: ");
+            myObject.inheritedProperty = Console.ReadLine();
+
+            recordList.Add(myObject);
+        }
+
+        // Print out the list of records
+        Console.WriteLine("List of records:");
+        foreach (var record in recordList)
+        {
+            Console.WriteLine($"myProperty: {record.myProperty}, inheritedProperty: {(record is MyInheritedClass obj ? obj.inheritedProperty : "N/A")}");
+        }
+    }
+}
+
+class MyBaseClass
+{
+    public string myProperty { get; set; }
+}
+
+class MyInheritedClass : MyBaseClass
+{
+    public string inheritedProperty { get; set; }
+}
+
